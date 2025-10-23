3d7a7086a023a4d5f91cc7b903b837089e816d2f
nirvair09 <chandradeep262003@gmail.com>
2025-10-23 11:11:34 +0530

    About added

---
+++ frontend/src/components/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About(){
+    return(
+        <div className="container-fluid p-5">
+            <h1>Inventory management System-crud</h1>
+            <p1> its a practice project small yet containing all major concepts</p1>
+        </div>
+    )
+}
