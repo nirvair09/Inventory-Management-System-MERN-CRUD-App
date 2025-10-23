0f0a0eb353b94bd4b4041e38a9b35dc12537b0f4
nirvair09 <chandradeep262003@gmail.com>
2025-10-23 10:51:35 +0530

    inertProduct route added

---
+++ backend/Routes/router.js
@@ -0,0 +1,28 @@
+const express= require(express);
+
+const router=express.Router();
+
+const products=require('../Models/Products');
+
+//inserting data
+
+router.post("/insertProduct",async(req,res)=>{
+    const {ProductName, ProductPrice, ProductBarCode}=req.body();
+
+    try {
+        const pre = await products.findOne({ProductBarCode: ProductBarCode})
+        console.log(pre);
+
+        if(pre){
+            res.status(422).json("Product Already Present");
+        }else{
+            const addProduct = new products({ ProductName, ProductPrice, ProductBarcode })
+            
+            await addProduct.save();
+            res.status(200).json(addProduct);
+            console.log(addProduct);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+})
